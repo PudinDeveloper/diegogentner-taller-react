04cc2f20451196fb20e85c8092a3a3d88ea004bb
dagp-ucol <dgentner@ucol.mx>
2022-06-29 23:06:20 -0500

    Resolved Issues and added classroom code

--- 00-demo-taller-react/src/main.jsx
+++ 00-demo-taller-react/src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
 import './index.css'
-// import bootstrap from 'bootstrap'
 
 const HelloWorld = (props) => {
   return (
@@ -17,20 +15,33 @@ const App = () => {
   const now = new Date();
   const a = 10;
   const b = 20;
+  const myList = [
+    'Primero',
+    'Segundo'
+  ]
 
   return (
     
   <div>
       <HelloWorld title="Adele es la mejor" />
+      <List items = {myList}/>
       <p>It is Now {now.toString()}</p>
       <p style={{color: a >= 10 ? "red" : "blue"}}>a + b is {a + b}</p>
   </div>
   )
 }
 
-const List = () => {
-  const myList
-}
+const List = ({items}) => ( //si utilizo parentesis es directamente
+
+    <ul>
+      {items.map(item => {
+        return (
+          <li>{item}</li>
+        )
+      })}
+    </ul>
+  
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- 01-ejercicio/src/main.jsx
+++ 01-ejercicio/src/main.jsx
@@ -1,29 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
-import './index.css'
 
+// App Const allow to execute de React App.
 const App = () => {
+
+  //Variable Declaration
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentlas of React';
+  const part1 = 'Fundamentals of React';
   const exercises1 = 10;
   const part2 = 'Using props to pass data';
   const exercises2 = 7;
   const part3 = 'State of component';
   const exercises3 = 14;
 
+  // Return from App const 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
-
-      <p>Number of exercises: {exercises1 + exercises2 + exercises3}</p>
+      <Header course = {course} />
+      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+      <Total total = {exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
 
+/////////////////////////////////////////////////////////// 
+// Props allows to share information between components. //
+///////////////////////////////////////////////////////////
+
+// Header const that appears in App return information.
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+// Content const that appears in App return information.
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part1}: {props.exercises1}</p>
+      <p>{props.part2}: {props.exercises2}</p>
+      <p>{props.part3}: {props.exercises3}</p>
+    </div>
+  )
+}
+
+// Total const that appears in App return information.
+const Total = (props) => {
+  return (
+    <p>Number of exercises: {props.total} </p>
+  )
+}
+
+// React Render
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
