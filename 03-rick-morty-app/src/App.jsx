aaa02fe521d2156b768ef7653f0c0e722a6c03d6
dagp-ucol <dgentner@ucol.mx>
2022-07-01 10:01:50 -0500

    Rick Morty App initialized

---
+++ 03-rick-morty-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='container mx-auto'>
+      <h2 className="text-indigo-600 text-center uppercase font-bold">
+        Welcome
+      </h2>
+      <h1 className='font-black text-2xl font-b text-center'>
+        Rick &amp; Morty App
+      </h1>
+    </div>
+  )
+}
+
+export default App
