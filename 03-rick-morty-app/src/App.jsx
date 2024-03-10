f36a92a7396bd9957992768b0c973c088224e18f
PudinDeveloper <diegoalejandropolanco@gmail.com>
2024-03-10 16:22:18 -0600

    Add rick and morty api information and images

--- 03-rick-morty-app/src/App.jsx
+++ 03-rick-morty-app/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then(response => response.json())
+      .then(data => setCharacters(data.results));
+  }, []);
+
   return (
     <div className='container mx-auto'>
       <h2 className="text-indigo-600 text-center uppercase font-bold">
-        Welcome
+        Semana 3 del Club
       </h2>
       <h1 className='font-black text-2xl font-b text-center'>
-        Rick &amp; Morty App
+        Rick &amp; Morty App by Diego Gentner and Carla Rodriguez | Simple API Example
       </h1>
+      {characters.map(character => (
+        <div key={character.id}>
+          <h2>{character.name}</h2>
+          <img src={character.image} alt={character.name} />
+        </div>
+      ))}
     </div>
   )
 }
